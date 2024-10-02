ddd72051021cd5d2031cce26ff270779150a5426
sina <sina.khosravi.ac@gmail.com>
2024-10-03 01:07:00 +0330

    add run from command line bash script

---
+++ ship-debug.sh
@@ -0,0 +1,7 @@
+gradle build
+gradle installDebug
+adb shell am force-stop come.securelight.secureshellv
+adb shell am start -n com.securelight.secureshellv/com.securelight.secureshellv.MainActivity
+sleep 1
+adb logcat --pid=$(adb shell pidof -s com.securelight.secureshellv) | grep -v "Empty SMPTE"
+
